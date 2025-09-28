38b1892e3d2f4f56baf0a24106cd5562dcfce85b
Mehrinwagle <mehrinwagle26@gmail.com>
2025-09-28 16:48:29 +0530

    hybrid inheritance

--- hybrid.py
+++ hybrid.py
@@ -4,27 +4,27 @@ class Person:
     def __init__(self,name):
         self.name=name
     def show_name(self):
-        print("Name",self.name)
+        print("Name :",self.name)
 #derived class 1(single inheritance)
 class Employee(Person):
     def __init__(self,name,emp_id):
         super().__init__(name)
         self.emp_id=emp_id
     def show_employee(self):
-        print("Emp_id",self.emp_id)
+        print("Emp_id :",self.emp_id)
 #derived class 2(multiple inheritance)
 class Admin(Employee):
     def __init__(self,name,emp_id,role):
         super().__init__(name,emp_id)
         self.role=role
     def show_role(self):
-        print("role",self.role)
+        print("role :",self.role)
 #another base class(hireachal part)
 class Contact:
     def __init__(self,email):
         self.email=email
     def show_email(self):
-        print("email",self.email)
+        print("email :",self.email)
 #hybrid inheritance: admin contact inherits from admin and contact(multiple+multilevel)
 class AdminContact(Admin,Contact):
     def __init__(self,name,emp_id,role,email):
